ff0bcd3c50b12cda983214356f65c49746bcdf4a
deepankverma <deepankverma1@gmail.com>
2015-09-17 23:18:28 +0530

    created js

---
+++ assets/js/functions.js
@@ -0,0 +1,13 @@
+var pContainerHeight = $('.headerwrap').height();
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll <= pContainerHeight) {
+
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+    });
+  }
+ });
